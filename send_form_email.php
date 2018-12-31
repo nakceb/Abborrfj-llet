5244b8ff95c14cba2305edcb58642991a7dc90a1
nakceb <fstenb@kth.se>
2018-12-31 12:48:21 +0100

    Ändrar till pappas mail

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "filip.stenbeck@hotmail.com";
+    $email_to = "jsstenbeck@gmail.com";
     $email_subject = "Kontakta-oss-Abborfjallet";
  
     function died($error) {
