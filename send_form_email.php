2bbc72296923486790723c7c9baaa8a2676b51ac
nakceb <fstenb@kth.se>
2019-01-16 21:05:01 +0100

    update reply to

--- send_form_email.php
+++ send_form_email.php
@@ -75,7 +75,7 @@ if(isset($_POST['email'])) {
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
+'Reply-To: '.$email."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
 ?>
